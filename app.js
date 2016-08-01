7042f186a281cdad352d10f377f4a7128b9c8ea2
Sho <daxjps@gmail.com>
2016-08-02 03:13:05 +0900

    dummy custum

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var http = require( 'http' ); // HTTPモジュール読み込み
 var socketio = require( 'socket.io' ); // Socket.IOモジュール読み込み
 var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
 
-// 3000番ポートでHTTPサーバーを立てる
+// 3001番ポートでHTTPサーバーを立てる
 var server = http.createServer( function( req, res ) {
     res.writeHead(200, { 'Content-Type' : 'text/html' }); // ヘッダ出力
     res.end( fs.readFileSync('./index.html', 'utf-8') );  // index.htmlの内容を出力
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 3001);
 
 // サーバーをソケットに紐付ける
 var io = socketio.listen( server );
@@ -15,14 +15,8 @@ var io = socketio.listen( server );
 io.sockets.on( 'connection', function( socket ) {
 
     // クライアントからサーバーへ メッセージ送信ハンドラ（自分を含む全員宛に送る）
-    socket.on( 'c2s_message', function( data ) {
+    socket.on( 'pointInput', function( data ) {
         // サーバーからクライアントへ メッセージを送り返し
-        io.sockets.emit( 's2c_message', { value : data.value } );
-    });
-
-    // クライアントからサーバーへ メッセージ送信ハンドラ（自分以外の全員宛に送る）
-    socket.on( 'c2s_broadcast', function( data ) {
-        // サーバーからクライアントへ メッセージを送り返し
-        socket.broadcast.emit( 's2c_message', { value : data.value } );
+        io.sockets.emit( 'point', { x : data.x, y : data.y} );
     });
 });
